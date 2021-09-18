36a684e2aca32256287de19e34d23675c0c9418b
Garrett George <garrett.michael.george@gmail.com>
2021-09-18 14:09:29 -0500

    refactor: extract multiple_of?/2

--- lib/project_euler/001_multiples_of_3_or_5.ex
+++ lib/project_euler/001_multiples_of_3_or_5.ex
@@ -1,14 +1,13 @@
 defmodule ProjectEuler.MultiplesOf3Or5 do
   defguard is_multiple_of(n, base) when rem(n, base) == 0
 
-  def multiple_of_3?(n) when is_multiple_of(n, 3), do: true
-  def multiple_of_3?(n) when is_integer(n), do: false
-
-  def multiple_of_5?(n) when is_multiple_of(n, 5), do: true
-  def multiple_of_5?(n) when is_integer(n), do: false
+  def multiple_of?(n, base) when is_multiple_of(n, base), do: true
+  def multiple_of?(n, _) when is_integer(n), do: false
+  def multiple_of_3?(n), do: multiple_of?(n, 3)
+  def multiple_of_5?(n), do: multiple_of?(n, 5)
 
   def list_multiples_of(n, base) do
     1..n
-    |> Enum.filter(&multiple_of_3?/1)
+    |> Enum.filter(&multiple_of?(&1, base))
   end
 end
