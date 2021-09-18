967636e4712d399090e685a7899d434da35737f0
Garrett George <garrett.michael.george@gmail.com>
2021-09-18 13:21:58 -0500

    multiple_of_3?/1 works for 3 or 9

--- lib/project_euler/001_multiples_of_3_or_5.ex
+++ lib/project_euler/001_multiples_of_3_or_5.ex
@@ -3,6 +3,6 @@ defmodule ProjectEuler.MultiplesOf3Or5 do
   end
 
   def multiple_of_3?(n) do
-    true
+    n == 3 or n == 9
   end
 end
--- test/project_euler/001_multiples_of_3_or_5_test.exs
+++ test/project_euler/001_multiples_of_3_or_5_test.exs
@@ -7,5 +7,13 @@ defmodule ProjectEuler.MultiplesOf3Or5Test do
     test "returns true for 3" do
       assert MultiplesOf3Or5.multiple_of_3?(3), "should return true for a multiple of 3"
     end
+
+    test "returns true for 9" do
+      assert MultiplesOf3Or5.multiple_of_3?(9), "should return true for a multiple of 3"
+    end
+
+    test "returns false for 5" do
+      refute MultiplesOf3Or5.multiple_of_3?(5), "should return false for a multiple of 3"
+    end
   end
 end
