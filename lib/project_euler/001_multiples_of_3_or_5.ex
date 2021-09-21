a66a84457b1b563f8e3386e056a0afbd2dcc1415
Garrett George <garrett.michael.george@gmail.com>
2021-09-20 20:50:25 -0500

    feat: list_multiples_of/3 takes a list of multiples

--- lib/project_euler/001_multiples_of_3_or_5.ex
+++ lib/project_euler/001_multiples_of_3_or_5.ex
@@ -1,10 +1,10 @@
 defmodule ProjectEuler.MultiplesOf3Or5 do
-  @moduledoc """
-  > If we list all the natural numbers below 10 that are multiples of 3 or 5, we
-  > get 3, 5, 6 and 9. The sum of these multiples is 23.
+  @moduledoc ~S"""
+  "If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+  get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-  > Find the sum of all the multiples of 3 or 5 below 1000.
-  > - Project Euler Problem 1 (https://projecteuler.net/problem=1)
+  Find the sum of all the multiples of 3 or 5 below 1000."
+  - Project Euler Problem 1 (https://projecteuler.net/problem=1)
   """
   defguard is_multiple_of(n, base) when rem(n, base) == 0
 
@@ -15,15 +15,22 @@ defmodule ProjectEuler.MultiplesOf3Or5 do
 
   @spec list_multiples_of(non_neg_integer, non_neg_integer) :: [non_neg_integer]
   def list_multiples_of(n, base) when is_integer(n) and is_integer(base) do
-    1..n
+    1..(n - 1)
     |> Enum.filter(&multiple_of?(&1, base))
   end
 
+  def list_multiples_of(n, base_list) when is_integer(n) and is_list(base_list) do
+    base_list
+    |> Enum.reduce([], fn base, acc ->
+      acc ++ list_multiples_of(n, base)
+    end)
+    |> Enum.sort()
+  end
+
   def sum_multiples_of_3_or_5(n) do
-    if n == 20 do
-      78
-    else
-      23
-    end
+    n
+    |> list_multiples_of(3)
+    |> Enum.concat(list_multiples_of(n, 5))
+    |> Enum.sum()
   end
 end
