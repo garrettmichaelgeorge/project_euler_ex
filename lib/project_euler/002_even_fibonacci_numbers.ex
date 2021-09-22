7d26dc1eb6430d74c9c4a6f52e8b8d940c70202a
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:31:48 -0500

    refactor: get Fibonacci term recursively

--- lib/project_euler/002_even_fibonacci_numbers.ex
+++ lib/project_euler/002_even_fibonacci_numbers.ex
@@ -1,8 +1,7 @@
 defmodule ProjectEuler.Fibonacci do
   def at(1), do: 1
   def at(2), do: 2
-  def at(3), do: 3
-  def at(4), do: 5
+  def at(n), do: at(n - 1) + at(n - 2)
 
   def sequence_up_to(n) do
     case n do
