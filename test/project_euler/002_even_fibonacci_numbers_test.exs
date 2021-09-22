a2e7850a59b727a5d5547bc4e2a54892eafd87ac
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:34:04 -0500

    feat: generate 6th Fibonacci term

--- test/project_euler/002_even_fibonacci_numbers_test.exs
+++ test/project_euler/002_even_fibonacci_numbers_test.exs
@@ -7,6 +7,26 @@ defmodule ProjectEuler.FibonacciTest do
     assert Fibonacci.at(1) == 1
   end
 
+  test "generates the second term" do
+    assert Fibonacci.at(2) == 2
+  end
+
+  test "generates the third term" do
+    assert Fibonacci.at(3) == 3
+  end
+
+  test "generates the fourth term" do
+    assert Fibonacci.at(4) == 5
+  end
+
+  test "generates the fifth term" do
+    assert Fibonacci.at(5) == 8
+  end
+
+  test "generates the sixth term" do
+    assert Fibonacci.at(6) == 13
+  end
+
   test "generates the first term of the Fibonacci sequence" do
     assert Fibonacci.sequence_up_to(1) == [1]
   end
