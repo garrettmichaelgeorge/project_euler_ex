5a5d49906862b5fdb6624331ddc36c2ae6dd6dad
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 19:49:42 -0500

    refactor: remove obsolete code from MultiplesOf3Or5

--- lib/project_euler/001_multiples_of_3_or_5.ex
+++ lib/project_euler/001_multiples_of_3_or_5.ex
@@ -28,16 +28,6 @@ defmodule ProjectEuler.MultiplesOf3Or5 do
     |> Enum.filter(&multiple_of?(&1, divisor))
   end
 
-  @spec list_multiples_of(non_neg_integer, [non_neg_integer]) :: [non_neg_integer]
-  def list_multiples_of(n, divisors) when is_integer(n) and is_list(divisors) do
-    divisors
-    |> Enum.reduce([], fn divisor, acc ->
-      acc ++ list_multiples_of(n, divisor)
-    end)
-    |> Enum.sort()
-    |> Enum.uniq()
-  end
-
   @spec multiple_of?(non_neg_integer, non_neg_integer) :: boolean
   def multiple_of?(n, divisor) when is_multiple_of(n, divisor), do: true
   def multiple_of?(n, divisor) when is_integer(n) and is_integer(divisor), do: false
