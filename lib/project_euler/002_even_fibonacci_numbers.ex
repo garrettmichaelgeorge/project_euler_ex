03fa04e83ac46563cf3177fd0dd1152940d9b68a
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:03:14 -0500

    refactor: use case statement in fibonacci_sequence/1

--- lib/project_euler/002_even_fibonacci_numbers.ex
+++ lib/project_euler/002_even_fibonacci_numbers.ex
@@ -1,9 +1,8 @@
 defmodule ProjectEuler.EvenFibonacciNumbers do
   def fibonacci_sequence(n) do
-    if n == 2 do
-      [1, 2]
-    else
-      [1]
+    case n do
+      2 -> [1, 2]
+      1 -> [1]
     end
   end
 end
