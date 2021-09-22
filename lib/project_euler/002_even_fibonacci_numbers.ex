21c25a5397bdf56549ad34ef2c0b6a747a31503a
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:02:13 -0500

    generate the first 2 terms of the fibonacci sequence

--- lib/project_euler/002_even_fibonacci_numbers.ex
+++ lib/project_euler/002_even_fibonacci_numbers.ex
@@ -1,2 +1,9 @@
 defmodule ProjectEuler.EvenFibonacciNumbers do
+  def fibonacci_sequence(n) do
+    if n == 2 do
+      [1, 2]
+    else
+      [1]
+    end
+  end
 end
--- test/project_euler/002_even_fibonacci_numbers_test.exs
+++ test/project_euler/002_even_fibonacci_numbers_test.exs
@@ -2,4 +2,12 @@ defmodule ProjectEuler.EvenFibonacciNumbersTest do
   use ExUnit.Case, async: true
 
   alias ProjectEuler.EvenFibonacciNumbers
+
+  test "generates the first term of the Fibonacci sequence" do
+    assert EvenFibonacciNumbers.fibonacci_sequence(1) == [1]
+  end
+
+  test "generates the first 2 terms of the Fibonacci sequence" do
+    assert EvenFibonacciNumbers.fibonacci_sequence(2) == [1, 2]
+  end
 end
