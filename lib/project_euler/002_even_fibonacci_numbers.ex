59fe86f1186d60ba43df285a6fc77e1fd7ddc76b
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:20:32 -0500

    refactor: introduce fibonacci base case of 1

--- lib/project_euler/002_even_fibonacci_numbers.ex
+++ lib/project_euler/002_even_fibonacci_numbers.ex
@@ -1,8 +1,15 @@
 defmodule ProjectEuler.EvenFibonacciNumbers do
+  def fibonacci(1), do: 1
+
   def fibonacci_sequence(n) do
     case n do
       4 ->
         [1, 2, 3, 5]
+      1 ->
+        1
+        |> fibonacci()
+        |> List.wrap()
+          
       n ->
         1..n |> Enum.to_list()
     end
