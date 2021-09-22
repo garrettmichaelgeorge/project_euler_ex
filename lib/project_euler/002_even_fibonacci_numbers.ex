784ca6b8ac87e67341058a30ebc0ed4622792021
Garrett George <garrett.michael.george@gmail.com>
2021-09-21 20:22:01 -0500

    refactor: extract fibonacci base case of 2

--- lib/project_euler/002_even_fibonacci_numbers.ex
+++ lib/project_euler/002_even_fibonacci_numbers.ex
@@ -1,15 +1,20 @@
 defmodule ProjectEuler.EvenFibonacciNumbers do
   def fibonacci(1), do: 1
+  def fibonacci(2), do: 2
 
   def fibonacci_sequence(n) do
     case n do
       4 ->
         [1, 2, 3, 5]
+
+      2 ->
+        [1, 2]
+
       1 ->
         1
         |> fibonacci()
         |> List.wrap()
-          
+
       n ->
         1..n |> Enum.to_list()
     end
